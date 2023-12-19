0208d7e44a3bfaef7a626dbb308c116fcf45e859
nayyareen <147015988+nayyareen@users.noreply.github.com>
2023-12-19 19:53:17 +0530

    Update pattern.py

--- pattern.py
+++ pattern.py
@@ -10,3 +10,10 @@ for i in range(1,n+1):
 	for i in range(i):
 		print(i,end="")
 	print()
+strr="harrwy"
+print(strr[0])
+print(strr[1])
+print(strr[2])
+print(strr[3])
+print(strr[4])
+print(strr[5])
